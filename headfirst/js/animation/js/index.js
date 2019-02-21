8fb6bf9c738db7d324321c065f71475d457c9353
maybetm <sergey.zebzeev@gmail.com>
2019-02-21 23:40:24 +0500

    les1

---
+++ headfirst/js/animation/js/index.js
@@ -0,0 +1,10 @@
+
+function onClickImg ()
+{
+	var username = prompt("Как вас зовут?", "Введите ваше имя.");
+	
+	if (username) {
+		alert ("Рад вас видеть, " + username + ".");
+		document.getElementById("").src = "img/333967.jpg"
+	}
+}
