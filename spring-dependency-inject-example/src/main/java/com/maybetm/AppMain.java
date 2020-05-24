73596a7bfaf5106fe497bae439ed23918aea005b
zebzeev-sv <sergey.zebzeev@gmail.com>
2020-05-24 21:04:47 +0500

    init

--- spring-dependency-inject-example/src/main/java/com/maybetm/AppMain.java
+++ spring-dependency-inject-example/src/main/java/com/maybetm/AppMain.java
@@ -1,19 +1,20 @@
 package com.maybetm;
 
-import com.maybetm.configuration.Config;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author zebzeev-sv
  * @version 17.05.2020 15:13
  */
+@ComponentScan ("com.maybetm")
 public class AppMain
 {
 
   public static void main(String... args)
   {
-    ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppMain.class);
 
     UserServiceController controller = context.getBean(UserServiceController.class);
     controller.print();
